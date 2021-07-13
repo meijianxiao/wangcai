9602627bea37229aae52e6733323e7176775d087
meijianxiao <1003409251@qq.com>
2021-07-13 08:01:13 +0800

    可以选中或取消 tag

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+    <ul class="current">
+      <li v-for="tag in dataSource" :key="tag" :class="{Selected:selectedTags.indexOf(tag)>=0}" @click="toggle(tag)">{{tag}}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {Component,Prop} from "vue-property-decorator";
+@Component
+export default  class Tags extends Vue{
+  @Prop() dataSource :string[] | undefined
+  selectedTags :string[] = []
+  toggle(tag:string){
+    const index = this.selectedTags.indexOf(tag)
+    if(index>=0){
+      this.selectedTags.splice(index)
+    }else{
+      this.selectedTags.push(tag)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+  > .current {
+    display: flex;
+
+    > li {
+      $bg:#d8d9d9;
+      background: $bg;
+      height: 24px; // 只有内容是一行的时候
+      line-height: 24px;
+      border-radius: (24px/2);
+      padding: 0 16px;
+      margin-right: 12px;
+      &.Selected{
+        background: darken($bg,50%);
+        color: white;
+      }
+    }
+  }
+
+  > .new {
+    padding-top: 16px;
+
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
+
+</style>
